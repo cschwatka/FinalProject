2092855e624e19bfb20899309d1eabd1484d4b4d
DustinMC1802 <dustinmcollier@gmail.com>
2022-05-24 12:32:21 -0700

    Configured hibernate, tested User entity

--- JPABookNook/src/test/java/com/skilldistillery/booknook/entities/UserTest.java
+++ JPABookNook/src/test/java/com/skilldistillery/booknook/entities/UserTest.java
@@ -2,33 +2,50 @@ package com.skilldistillery.booknook.entities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class UserTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private User user;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPABookNook");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		user = em.find(User.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		user = null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("Test basic user entity mapping")
+	void test_basic_user_entity_mapping() {
+		assertNotNull(user);
+		assertEquals("admin", user.getUsername());
 	}
 
 }
