a057d0b6a9f388371cd5c245681c7c9219ca00eb
Chase Schwatka <cschwatka@gmail.com>
2022-05-25 10:50:19 -0600

    nook bootsrap smoke test

--- ngBookNook/src/app/app.module.ts
+++ ngBookNook/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NookComponent } from './components/nook/nook.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NookComponent
   ],
   imports: [
     BrowserModule,
