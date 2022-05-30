409e0b1ccbfdc9b8bc80bd4330df430669db407f
Dustin Jennings <dustinejennings@gmail.com>
2022-05-30 14:19:23 -0600

    updated language, fixed recrsion

---
+++ BookNook/src/main/java/com/skilldistillery/booknook/services/LanguageService.java
@@ -0,0 +1,10 @@
+package com.skilldistillery.booknook.services;
+
+import java.util.List;
+
+import com.skilldistillery.booknook.entities.Language;
+
+public interface LanguageService {
+
+	public List<Language> index();
+}
